1a7869840170c043fe39fff7498beb1e3d6d3599
jeev.jo <jeevsspace@gmail.com>
2022-11-09 15:26:38 +0530

    MAD

---
+++ MAD/8. array adapter/ArrayAdapterExample/app/src/main/java/com/example/arrayadapterexample/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.arrayadapterexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    // Array of strings...
+    ListView simpleList;
+    String animalList[] = {"Lion","Tiger","Monkey","Elephant","Dog","Cat","Camel"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        simpleList = (ListView) findViewById(R.id.simpleListView);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_list_view, R.id.textView, animalList);
+        simpleList.setAdapter(arrayAdapter);
+    }
+
+}
